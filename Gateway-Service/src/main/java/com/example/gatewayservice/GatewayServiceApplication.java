14cebcf0f474cb80fcb49c942ab100601a80c73a
roldanordonez1990 <roldanordonez.francisco@gmail.com>
2022-02-08 18:13:35 +0100

    ggg

--- Gateway-Service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ Gateway-Service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,11 +1,13 @@
 package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+
 @EnableEurekaClient
+@SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
